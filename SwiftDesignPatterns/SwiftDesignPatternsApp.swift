9b18e5ce26aa380c42ed91aaaaaea65dc4c83178
Julio Cesar <js.cesar42@gmail.com>
2024-05-24 08:40:14 -0300

    Initial Commit

---
+++ SwiftDesignPatterns/SwiftDesignPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDesignPatternsApp.swift
+//  SwiftDesignPatterns
+//
+//  Created by Julio Cesar Pereira on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDesignPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
